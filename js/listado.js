4f73792746b4c835280805788ff97856581e7537
Pau Santarrosa <paulasantarrosa@gmail.com>
2020-08-09 18:51:07 -0300

    refactoring

--- js/listado.js
+++ js/listado.js
@@ -18,10 +18,10 @@ Listado.prototype.calificarRestaurant = function (id, calificacion) {
 
 //Dado un id, busca el objeto del listado que tiene ese id
 Listado.prototype.buscarRestaurante = function (id) {
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        if (this.restaurantes[i].id === id) {
-            return this.restaurantes[i]
-        }
+    //refactoring paso 6 Guia
+    var restaurant = this.restaurantes.find(restaurante => restaurante.id === id);
+    if(restaurant!=null){
+        return restaurant
     }
     return "No se ha encontrado ningún restaurant";
 }
